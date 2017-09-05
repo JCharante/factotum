348f1c37937664e55a0c99ec0837147783ad1ed0
John <git@jcharante.com>
2017-09-05 15:13:48 -0400

    update 1.1.1

--- factotum.js
+++ factotum.js
@@ -2,7 +2,7 @@
 var program = require('commander');
 
 program
-    .version('1.1.0')
+    .version('1.1.1')
     .command('music [action]', 'push to or pull from remote music repository')
     .command('configure [what]', 'configure your settings')
     .parse(process.argv);
